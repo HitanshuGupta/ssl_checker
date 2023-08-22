1c0d193eb48a97b7f6b979bd10ce9a57af0b1f31
Hitanshu <hitanshugupta212@gmail.com>
2023-08-22 16:37:08 +0530

    script file

---
+++ test2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#set -x  # Enable debugging mode
+
+SLACK_WEBHOOK_URL="https://hooks.slack.com/services/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"
+DOMAINS_FILE="/root/PearlThoughts/domains.txt"
+
+echo "Starting SSL Expiry Check"
+
+while IFS= read -r TARGET; do
+  [ -z "$TARGET" ] && continue
+  
+  expirationdate=$(date -d "$(: | openssl s_client -connect "$TARGET":443 -servername "$TARGET" 2>/dev/null \
+                                | openssl x509 -text \
+                                | grep 'Not After' \
+                                |awk '{print $4,$5,$7}')" '+%s')
+
+  current_epoch=$(date +%s)
+  remaining_days=$(( (expirationdate - current_epoch) / 86400 ))
+
+  message="SSL Expiry Alert
+   * Domain: $TARGET
+   * Warning: The SSL certificate for $TARGET will expire in $remaining_days days."
+
+  echo "Sending Alert for Domain: $TARGET"
+  curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$message\"}" "$SLACK_WEBHOOK_URL"
+done < "$DOMAINS_FILE"
+
+echo "SSL Expiry Check Completed"
+
+#set +x  # Disable debugging mode
+
